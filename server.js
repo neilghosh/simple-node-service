df0fc376738de0e80025dae4b2e646033707ccbe
Neil Ghosh <neil.ghosh@gmail.com>
2022-09-21 18:16:38 +0000

    Commit

--- server.js
+++ server.js
@@ -3,26 +3,41 @@ import { fetchData } from "./webproxy.js";
 import { dbConnect } from "./db.js";
 
 const app = express();
-app.use('/static',express.static('public'))
+app.use('/static', express.static('public'))
 app.get("/", (req, res) => {
-  console.log(`Request Came ${req.ip}`);
-  fetchData()
-    .then((result) => {
-      console.log(result.statusCode);
-      res.write('Hyderabad CCD Success '+result.statusCode);
-    })
-    .catch((result) => {
-      console.log('Failed '+result.statusCode);
-      res.send(500);
-    })
-    .finally(() => {
-      console.log(`Request End`);
-      res.end(); //end the response
-    });
+    console.log(`Request Came ${req.ip}`);
+    fetchData()
+        .then((result) => {
+            console.log(result.statusCode);
+            res.write('Hyderabad CCD Success ' + result.statusCode);
+        })
+        .catch((result) => {
+            console.log('Failed ' + result.statusCode);
+            res.send(500);
+        })
+        .finally(() => {
+            console.log(`Request End`);
+            res.end(); //end the response
+        });
 });
 app.get("/hit", (req, res) => {
     console.log(`Request Came ${req.ip}`);
-    dbConnect();
-  });
+    // dbConnect().then((result) => {
+    //     console.log(result);
+    //     res.write('DB Result' + result);
+    // })
+    //     .catch((result) => {
+    //         console.log('Failed ' + result);
+    //         res.send(500);
+    //     })
+
+    (async () => {
+        const result = await dbConnect();
+        console.log("Time with pool: " + JSON.stringify(result));
+        res.write(JSON.stringify(result));
+        res.end();
+        //res.send(200);
+      })();
+});
 
 export { app as server };
